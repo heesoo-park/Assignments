f8d633688d0ab9c8b21b08fcff15ec7f9388a1a5
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2023-12-01 16:51:43 +0900

    Update AddOperation.kt

--- 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv4/AddOperation.kt
+++ 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv4/AddOperation.kt
@@ -1,3 +1,9 @@
-class AddOperation: AbstractOperation {
-    override fun operation(n1: Int, n2: Int): Int = n1 + n2
+// 수정 버전
+class AddOperation: AbstractOperation() {
+    override fun operation(n1: Double, n2: Double): Double = n1 + n2
 }
+
+// 이전 버전
+// class AddOperation: AbstractOperation {
+//     override fun operation(n1: Int, n2: Int): Int = n1 + n2
+// }
