65238251fca337874c6ac1d94549fa1122b3cd4f
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2023-12-01 16:48:04 +0900

    Update AddOperation.kt

--- 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv3/AddOperation.kt
+++ 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv3/AddOperation.kt
@@ -1,3 +1,9 @@
+// 수정 버전 
 class AddOperation {
-    fun add(n1: Int, n2: Int) = n1 + n2
+    fun operation(n1: Double, n2: Double): Double = n1 + n2
 }
+
+// 이전 버전
+// class AddOperation {
+//     fun add(n1: Int, n2: Int) = n1 + n2
+// }
