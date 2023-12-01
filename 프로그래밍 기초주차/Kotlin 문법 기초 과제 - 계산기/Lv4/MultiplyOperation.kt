21b2997c3741cb831851317421039f4b5c3892d3
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2023-12-01 16:54:58 +0900

    Update MultiplyOperation.kt

--- 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv4/MultiplyOperation.kt
+++ 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv4/MultiplyOperation.kt
@@ -1,3 +1,9 @@
-class MultiplyOperation: AbstractOperation {
-    override fun operation(n1: Int, n2: Int): Int = n1 * n2
+// 수정 버전
+class MultiplyOperation: AbstractOperation() {
+    override fun operation(n1: Double, n2: Double): Double = n1 * n2
 }
+
+// 이전 버전
+// class MultiplyOperation: AbstractOperation {
+//     override fun operation(n1: Int, n2: Int): Int = n1 * n2
+// }
