2f76959ca90cf83f53f228439fad6792909f05b4
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2023-12-01 16:55:21 +0900

    Update SubstractOperation.kt

--- 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv4/SubstractOperation.kt
+++ 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv4/SubstractOperation.kt
@@ -1,3 +1,9 @@
-class SubstractOperation: AbstractOperation {
-    override fun operation(n1: Int, n2: Int): Int = n1 - n2
+// 수정 버전
+class SubstractOperation: AbstractOperation() {
+    override fun operation(n1: Double, n2: Double): Double = n1 - n2
 }
+
+// 이전 버전
+// class SubstractOperation: AbstractOperation {
+//     override fun operation(n1: Int, n2: Int): Int = n1 - n2
+// }
