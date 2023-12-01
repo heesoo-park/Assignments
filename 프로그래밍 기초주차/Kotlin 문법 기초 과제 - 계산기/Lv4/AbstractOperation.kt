78cb5c92957e7e1b91567b2edc1cf8c3c8b1ba27
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2023-12-01 16:51:16 +0900

    Update AbstractOperation.kt

--- 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv4/AbstractOperation.kt
+++ 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv4/AbstractOperation.kt
@@ -1,3 +1,9 @@
-interface AbstractOperation {
-    fun operation(n1: Int, n2: Int): Int
+// 수정 버전
+abstract class AbstractOperation {
+    abstract fun operation(n1: Double, n2: Double): Double
 }
+
+// 이전 버전
+// interface AbstractOperation {
+//     fun operation(n1: Int, n2: Int): Int
+// }
