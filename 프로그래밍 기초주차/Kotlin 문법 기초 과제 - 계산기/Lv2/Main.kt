a409cc791905d155a902a1341707c40b75520e93
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2023-12-01 16:45:10 +0900

    Update Main.kt

--- 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv2/Main.kt
+++ 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv2/Main.kt
@@ -1,7 +1,52 @@
+// 수정 버전
 fun main() = with(System.`in`.bufferedReader()) {
     val calculator = Calculator()
 
-    calculator.startMessage()
-    calculator.selectSymbol()
-    calculator.stopMessage()
+    while (true) {
+        println("원하는 연산을 선택해주세요.")
+        println("1. 더하기  2. 빼기  3. 곱하기  4. 나누기  5. 나머지  6. 종료")
+        var symbol = readln().toInt()
+        var n1: Double = 0.0
+        var n2: Double = 0.0
+
+        // 제시된 범위 바깥이면 다시 유효한 연산 선택 유도
+        while (symbol > 5 || symbol <= 0) {
+            println("에러 : 유효하지 않은 연산입니다.")
+            println("다시 연산을 선택해주세요.")
+            println("1. 더하기  2. 빼기  3. 곱하기  4. 나누기  5. 나머지  6. 종료")
+            symbol = readln().toInt()
+        }
+
+        if (symbol == 6) break
+
+        if (calculator.accNum == Double.MIN_VALUE) {
+            println("두 수를 입력해주세요(공백으로 구분)")
+            val input = readln().split(" ")
+            n1 = input[0].toDouble()
+            n2 = input[1].toDouble()
+        } else {
+            println("숫자 하나를 입력해주세요")
+            n1 = calculator.accNum
+            n2 = readln().toDouble()
+        }
+
+
+        when (symbol) {
+            1 -> println("계산결과 : ${calculator.add(n1, n2)}")
+            2 -> println("계산결과 : ${calculator.substract(n1, n2)}")
+            3 -> println("계산결과 : ${calculator.multiply(n1, n2)}")
+            4 -> println("계산결과 : ${calculator.divide(n1, n2)}")
+            5 -> println("계산결과 : ${calculator.modulo(n1, n2)}")
+            else -> println("ERROR")
+        }
+    }
 }
+
+// 이전 버전
+// fun main() = with(System.`in`.bufferedReader()) {
+//     val calculator = Calculator()
+
+//     calculator.startMessage()
+//     calculator.selectSymbol()
+//     calculator.stopMessage()
+// }
