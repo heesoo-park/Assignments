9c666242f7dbc4b24f407be9d67463d757a8948c
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2023-12-01 16:48:25 +0900

    Update DivideOperation.kt

--- 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv3/DivideOperation.kt
+++ 프로그래밍 기초주차/Kotlin 문법 기초 과제 - 계산기/Lv3/DivideOperation.kt
@@ -1,3 +1,14 @@
+// 수정 버전
 class DivideOperation {
-    fun divide(n1: Int, n2: Int) = if (n2 != 0) n1 / n2 else Int.MIN_VALUE
+    fun operation(n1: Double, n2: Double): Double {
+        require(n2 != 0.0) {
+            ArithmeticException("Divide by Zero")
+        }
+        return n1 / n2
+    }
 }
+
+// 이전 버전
+// class DivideOperation {
+//     fun divide(n1: Int, n2: Int) = if (n2 != 0) n1 / n2 else Int.MIN_VALUE
+// }
