5173c107d65e3da5e6eaf1360007d1b3803b79cf
heesoo-park <80674868+heesoo-park@users.noreply.github.com>
2023-12-05 17:11:49 +0900

    Create ReservationPerson.kt

---
+++ 프로그래밍 기초주차/Kotlin 문법 심화 과제 - 호텔 예약 프로그래밍/ReservationPerson.kt
@@ -0,0 +1,7 @@
+data class ReservationPerson(
+    val name: String,       // 이름
+    var roomNum: Int,       // 방 번호
+    var checkIn: String,    // 체크인
+    var checkOut: String,   // 체크아웃
+    val money: Int          // 소지금
+)
